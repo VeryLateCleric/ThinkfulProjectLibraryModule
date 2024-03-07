428196befe35a1bef339054a4d3cfa9ae4f554fd
Kenneth <kennethyoung.2960@gmail.com>
2024-03-07 17:50:35 -0600

    9 of 20 tests passing

--- Project_Local_library_Node_18_1/public/src/accounts.js
+++ Project_Local_library_Node_18_1/public/src/accounts.js
@@ -1,8 +1,19 @@
 // Accounts
 
-function findAccountById(accounts, id) {}
+function findAccountById(accounts, id) {
+  return accounts.find(account => account.id === id);
+}
 
-function sortAccountsByLastName(accounts) {}
+function sortAccountsByLastName(accounts) {
+  // slice the names of the accounts up and sort them by last name after making all letter lowercase for comparing equally
+  return accounts.slice().sort((a, b) => {
+    //  make variable extracting a lowercase last name
+    const lastNameA = a.name.last.toLowerCase();
+    const lastNameB = b.name.last.toLowerCase();
+    // compoare using localeCompare
+    return lastNameA.localeCompare(lastNameB);
+  })
+}
 
 function getTotalNumberOfBorrows(account, books) {}
 
