94ae95eb7d02ae7fb5ca725b0e095f48df537f8f
Kenneth <kennethyoung.2960@gmail.com>
2024-03-07 18:25:42 -0600

    16 of 20 tests passing with some errors

--- Project_Local_library_Node_18_1/public/src/books.js
+++ Project_Local_library_Node_18_1/public/src/books.js
@@ -8,9 +8,27 @@ function findBookById(books, id) {
   return books.find(book => book.id === id);
 }
 
-function partitionBooksByBorrowedStatus(books) {}
+function partitionBooksByBorrowedStatus(books) {
+// filter if a book is not returned and if a book is returned, then return those results
+  const borrowedBooks = books.filter(book => !book.borrows[0].returned);
+  const returnedBooks = books.filter(book => book.borrows[0].returned);
+  return [borrowedBooks, returnedBooks];
+}
 
-function getBorrowersForBook(book, accounts) {}
+function getBorrowersForBook(book, accounts) {
+  // use map to check all borrow records in borrows array
+  // for each, find the account that borrowed
+  return book.borrows
+  .map(borrow => {
+    const account = accounts.find(account => account.id === borrow.id);
+    return {
+      ...borrow,
+      ...account,
+    };
+  })
+  // Now take the first 10 elemnents of the array so we end up with 10
+  .slice(0, 10);
+}
 
 module.exports = {
   findAuthorById,
